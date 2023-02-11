8a5770a3227f47c60ec1f7a2a48872308f2235d6
bbtit <the8butterfly8dream@gmail.com>
2023-02-11 22:23:01 +0900

    💄 footerのレイアウトを設定

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,14 @@
-import React from 'react'
 import Logo from 'components/logo'
+import styles from 'styles/footer.module.css'
 
 export default function Footer() {
   return (
-    <footer>
-      <Logo />
+    <footer className={styles.wrapper}>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
+      
     </footer>
   )
 }
